73e3c903f365eb019b934f53056246b82cb8edd7
Fabian Torres <fabiantorreees@gmail.com>
2024-06-06 15:35:27 -0400

    some changes

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import ScreenSaver from "../../../components/ScreenSaver";
+import Link from "next/link";
 
-function ScreenSaverExercise() {
+const COLORS = ["lavender", "slateblue", "hotpink", "white"];
+
+function ScreenSaverIndexPage() {
   return (
     <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+      <p>Choose your color:</p>
+      <ul>
+        {COLORS.map((color, index) => (
+          <li key={index}>
+            <Link href={`/exercises/01-screensaver/${color}`}>{color}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverIndexPage;
